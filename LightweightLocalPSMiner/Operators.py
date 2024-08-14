c79fdae3e619c0b46b1e318fbc27a19d5ec4f572
Giancarlo_Catalano <g.a.catalano@stir.ac.uk>
2024-08-14 11:32:43 +0100

    Implemented the crowding operator for the local search.

--- LightweightLocalPSMiner/Operators.py
+++ LightweightLocalPSMiner/Operators.py
@@ -1,7 +1,11 @@
 import random
+from typing import Iterable, Any
 
 import numpy as np
 from pymoo.operators.sampling.rnd import FloatRandomSampling
+
+from Core.PS import PS, STAR
+from PSMiners.PyMoo.CustomCrowding import PyMooDecisionSpaceSequentialCrowding, PyMooCustomCrowding
 
 
 class LocalPSGeometricSampling(FloatRandomSampling):
@@ -20,7 +24,48 @@ class LocalPSGeometricSampling(FloatRandomSampling):
 
 
 
+class UnexplainedCrowdingOperator(PyMooCustomCrowding):
+    masks_to_avoid: list[np.ndarray]
+    sigma_shared: float
+    opt: Any
+
+
+    @classmethod
+    def ps_to_mask(cls, ps: PS) -> np.ndarray:
+        return ps.values != STAR
+    def __init__(self, to_avoid: Iterable[PS], sigma_shared: float = 0.5):
+        super().__init__()
+
+        self.masks_to_avoid = [self.ps_to_mask(ps) for ps in to_avoid]
+        self.sigma_shared = sigma_shared
+        self.opt = []
+
+    def distance_metric(self, x: np.ndarray, mask: np.ndarray):
+        overlap_count = np.sum(np.logical_and(x, mask), dtype=float)
+        fixed_count = (np.sum(x) + np.sum(mask))/2
+
+        if fixed_count < 1:
+            return 1
+        return 1 - (overlap_count / fixed_count)
+
+    def is_too_close(self, x, mask) -> bool:
+        return self.distance_metric(x, mask) < self.sigma_shared
+
+
+    def get_crowding_score(self, x: np.ndarray) -> float:
+        if len(self.masks_to_avoid) == 0:
+            return 1
+        amount_of_close = len([mask for mask in self.masks_to_avoid if self.is_too_close(x, mask)])
+        return 1 - (amount_of_close / len(self.masks_to_avoid))
+
+
+    def get_crowding_scores_of_front(self, all_F, n_remove, population, front_indexes) -> np.ndarray:
+        scores = np.array([self.get_crowding_score(population[index].X) for index in front_indexes])
+
+        self.opt = population[front_indexes]  # just to comply with Pymoo, ignore this
+        return scores
+
 # mutation should be BitFlipMutation(...)
 # crossover should be SimulatedBinaryCrossover(...)
 # selection should be tournamentSelection
-# crowding operator should be TODO
+# crowding operator should be UnexplainedCrowdingOperator
