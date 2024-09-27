40d9c45a6a6e3c6cf13f4a1078481559703baa17
Giancarlo Catalano <g.a.catalano@stir.ac.uk>
2024-09-27 10:39:54 +0100

    Updated the last few things in the DescriptorsManager class.

--- LCS/DifferenceExplainer/DescriptorsManager.py
+++ LCS/DifferenceExplainer/DescriptorsManager.py
@@ -2,12 +2,13 @@ from typing import Optional
 
 import pandas as pd
 
+import utils
 from BenchmarkProblems.BenchmarkProblem import BenchmarkProblem
 from Core.PS import PS
 from PSMiners.Mining import load_pss, write_pss_to_file
 
 
-class ControlPSsManager:
+class DescriptorsManager:
     optimisation_problem: BenchmarkProblem
     control_pss_file: str
     control_descriptors_table_file: str
@@ -18,11 +19,14 @@ class ControlPSsManager:
     control_samples_per_size_category: int
     sizes_for_which_control_has_been_generated: Optional[set[int]]
 
+    verbose: bool
+
     def __init__(self,
                  optimisation_problem: BenchmarkProblem,
                  control_pss_file: str,
                  control_descriptors_table_file: str,
-                 control_samples_per_size_category: int):
+                 control_samples_per_size_category: int,
+                 verbose: bool = False):
         self.optimisation_problem = optimisation_problem
 
         self.control_pss_file = control_pss_file
@@ -34,11 +38,16 @@ class ControlPSsManager:
         self.sizes_for_which_control_has_been_generated = None
         self.control_samples_per_size_category = control_samples_per_size_category
 
+        self.verbose = verbose
+
     @property
     def search_space(self):
         return self.optimisation_problem.search_space
 
     def load_from_files(self):
+        if self.verbose:
+            print(f"Loading ps control data from {self.control_pss_file} and {self.control_descriptors_table_file}")
+
         self.control_pss = load_pss(self.control_pss_file)
         self.control_descriptors_table = pd.read_csv(self.control_descriptors_table_file)
 
@@ -49,9 +58,13 @@ class ControlPSsManager:
         result["size"] = ps.fixed_count()
         return result
 
-    def generate_data_for_new_size_category(self, size_category: int):
+    def generate_data_for_new_size_category(self, size_category: int) -> pd.Dataframe:
         """Generates the new control pss and the descriptors.
-        It updates the internal control pss, the descriptor table and the 'sizes_for_which_control_has_been_generated"""
+        It updates the internal control pss, the descriptor table and the 'sizes_for_which_control_has_been_generated,
+        and returns the new rows generated"""
+
+        if self.verbose:
+            print(f"Generating control data for size category = {size_category}")
 
         new_control_pss = [PS.random_with_fixed_size(self.search_space, size_category)
                            for _ in range(self.control_samples_per_size_category)]
@@ -61,6 +74,7 @@ class ControlPSsManager:
         self.control_pss.extend(new_control_pss)
         self.control_descriptors_table = pd.concat([self.control_descriptors_table_file, new_property_rows])
         self.sizes_for_which_control_has_been_generated.add(size_category)
+        return new_property_rows
 
     def start_from_scratch(self):
         self.control_pss = []
@@ -68,5 +82,38 @@ class ControlPSsManager:
         self.sizes_for_which_control_has_been_generated = set()
 
     def write_to_files(self):
+        if self.verbose:
+            print(f"Storing the data in the files {self.control_pss_file}, {self.control_descriptors_table_file}")
         write_pss_to_file(pss=self.control_pss, file=self.control_pss_file)
         self.control_descriptors_table.to_csv(self.control_descriptors_table_file)
+
+
+    def get_table_rows_where_size_is(self, size: int) -> pd.DataFrame:
+        if not size in self.sizes_for_which_control_has_been_generated:
+            return self.generate_data_for_new_size_category(size_category=size)
+        else:
+            return self.control_descriptors_table[self.control_descriptors_table_file["size"] == size]
+
+
+
+    def get_percentiles_for_PS(self, ps: PS) -> dict[str, float]:
+        ps_size = ps.fixed_count()
+        table_rows = self.get_table_rows_where_size_is(ps_size)
+
+        ps_descriptors = self.get_descriptors_of_ps(ps)
+
+        def get_percentile_of_descriptor(descriptor_name: str) -> float:
+            descriptor_value = ps_descriptors[descriptor_name]
+            return utils.ecdf(descriptor_value, table_rows[descriptor_name])
+
+        return {descriptor_name: get_percentile_of_descriptor(descriptor_name)
+                for descriptor_name in ps_descriptors
+                if descriptor_name != "size"}
+
+
+
+
+
+
+
+
