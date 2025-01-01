7a76a292e523601a144a3a235a46d5e253d768f4
gian-mac <catalano.giancarlo.00@gmail.com>
2025-01-01 22:10:53 +0000

    Testing if getting the location of the script would help in accessing resources.

--- VarianceDecisionTree/compare_prediction_powers.py
+++ VarianceDecisionTree/compare_prediction_powers.py
@@ -14,7 +14,7 @@ from VarianceDecisionTree.AbstractDecisionTreeRegressor import AbstractDecisionT
 
 import platform
 
-if platform.system() in {"Darwin", "Windows"}:
+if platform.system() in {"Darwin", "Windows"}: # I KNOW THAT THIS IS DODGY, BUT THE LIBRARY WON'T WORK ON CONDOR
     from VarianceDecisionTree.IAIDecisionTree import IAIDecisionTree
 
 from VarianceDecisionTree.PSDecisionTree import PSDecisionTree, PSDecisionTreeRestrictedDepth
