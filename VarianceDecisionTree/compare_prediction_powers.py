59df0795ae9ad332047d36c90c52c438eb335d5d
gian-mac <catalano.giancarlo.00@gmail.com>
2025-01-06 13:50:10 +0000

    Ready for another round of condor, gathering data for the naive trees with absolutely default settings.

--- VarianceDecisionTree/compare_prediction_powers.py
+++ VarianceDecisionTree/compare_prediction_powers.py
@@ -16,7 +16,7 @@ from VarianceDecisionTree.AbstractDecisionTreeRegressor import AbstractDecisionT
 
 import platform
 
-if False and (platform.system() in {"Darwin", "Windows"}):  # I KNOW THAT THIS IS DODGY, BUT THE LIBRARY WON'T WORK ON CONDOR
+if (platform.system() in {"Darwin", "Windows"}):  # I KNOW THAT THIS IS DODGY, BUT THE LIBRARY WON'T WORK ON CONDOR
     from VarianceDecisionTree.IAIDecisionTree import IAIDecisionTree
 
 from VarianceDecisionTree.PSDecisionTree import PSDecisionTree, PSDecisionTreeRestrictedDepth
@@ -210,6 +210,56 @@ def gather_data_compare_dts():
 
     for iteration in range(repeats):
         single_run()
+
+
+def gather_data_compare_own():
+    problems = get_problems_with_names()
+    problems = dict(list(problems.items()))
+    pRef_methods = ["GA", "uniform"]
+    sample_size = 10000
+
+    depths = [2, 3, 4, 5, 6]
+    tree_dicts = []
+    tree_dicts.extend([{"kind": "ps",
+                        "ps_budget": ps_budget,
+                        "ps_population": 100,
+                        "depths": depths}
+                       for ps_budget in [1000, 2000, 5000]])
+
+    mode = "local"
+
+    def make_file_with_json_contents(json_dict):
+        json_file_name = os.path.join(destination_folder, "output_" + utils.get_formatted_timestamp() + ".json")
+        with open(json_file_name, "w") as file:
+            json.dump(json_dict, file, indent=4)
+
+    def single_run():
+        results = []
+
+        for problem_name, problem in problems.items():
+            for pRef_method in pRef_methods:
+                datapoint = get_datapoint_for_instance(problem_name=problem_name,
+                                                       problem=problem,
+                                                       tree_settings_list=tree_dicts,
+                                                       sample_size=sample_size,
+                                                       pRef_method=pRef_method,
+                                                       crash_on_error=False)
+                results.append(datapoint)
+
+        if mode == "local":
+            make_file_with_json_contents(results)
+        else:
+            print(json.dumps(results, indent=4))
+
+    if mode == "local":
+
+        repeats = 10
+        destination_folder = r"/Users/gian/PycharmProjects/PS-descriptors/resources/variance_tree_materials/compare_own_data" + utils.get_formatted_timestamp()
+        utils.make_directory(destination_folder)
+        print(f"Storing the results in {destination_folder}")
+
+        for iteration in range(repeats):
+            single_run()
 
 
 def gather_data_compare_own():
--- main.py
+++ main.py
@@ -172,7 +172,87 @@ def gather_data_compare_with_naive():
         single_run()
 
 
-gather_data_compare_with_naive()
+
+def gather_data_compare_iai():
+
+    problems = get_problems_with_names()
+    pRef_methods = ["uniform", "GA", "SA", "Tabu"]
+    sample_sizes = [10000, 30000]
+
+    depths = [2, 3, 4, 5, 6]
+    tree_dicts = []
+    tree_dicts.extend([{"kind": "iai",
+                        "cp": cp,
+                        "depths": depths}
+                       for cp in [0.25, 0.5, 0.75]])
+
+
+    mode = "local"
+    repeats = 5
+
+    debug = False
+    print_progress = True
+    if debug:
+        print("NOTE: using debug mode")
+        problems = dict(list(problems.items())[:1])
+        # pRef_methods = ["GA"]
+        # sample_size = 100
+        tree_dicts = tree_dicts[:1]
+        tree_dicts[0]["depths"] = [5]
+        sample_sizes = [500]
+
+    def make_file_with_json_contents(json_dict, seed):
+        json_file_name = os.path.join(destination_folder, f"output_{seed}_" + utils.get_formatted_timestamp() + ".json")
+        with open(json_file_name, "w") as file:
+            json.dump(json_dict, file, indent=4)
+
+    def single_run(seed: int):
+
+        results = []
+        warnings.warn(f"The seed is {seed}")
+
+        for problem_name, problem in problems.items():
+            for pRef_method in pRef_methods:
+                for sample_size in sample_sizes:
+                    if print_progress:
+                        warnings.warn(f"{problem_name = }, {pRef_method = }, {sample_size =}")
+                    datapoint = get_datapoint_for_instance(problem_name=problem_name,
+                                                           problem=problem,
+                                                           tree_settings_list=tree_dicts,
+                                                           sample_size=sample_size,
+                                                           pRef_method=pRef_method,
+                                                           crash_on_error=debug,
+                                                           seed = seed)
+                    results.append(datapoint)
+
+        if mode == "local":
+            make_file_with_json_contents(results, seed)
+        else:
+            print(json.dumps(results, indent=4))
+
+    if mode == "local":
+
+
+        destination_folder = r"/Users/gian/Desktop/CondorResults/VDT/compareown/iai_run_1"+utils.get_formatted_timestamp()
+        utils.make_directory(destination_folder)
+        print(f"Storing the results in {destination_folder}")
+
+        for iteration in range(repeats):
+            single_run(seed = iteration)
+
+    else:
+        # just print out the results to the console at the end
+        if len(sys.argv) < 2:
+            seed = random.randrange(10000)
+        else:
+            try:
+                seed = int(sys.argv[1])
+            except:
+                raise Exception(f"The second argument needs to be an integer, or a number! {sys.argv[1]} was provided")
+        single_run(seed)
+
+
+gather_data_compare_iai()
 
 
 
